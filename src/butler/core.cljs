9b721d62e7477909f819847f624f5ba9e68f6431
Christian Weilbach <christian_weilbach@web.de>
2013-10-28 00:59:23 +0100

    Build minimal node example with local CouchDB request.

--- src/butler/core.cljs
+++ src/butler/core.cljs
@@ -1,19 +1,42 @@
 (ns butler.core
-  (:require [cljs.nodejs :as nodejs]
-            [butler.communicator :as com]))
+  (:require [cljs.reader :refer [read-string]]
+            [cljs.core.async :as async :refer [chan <! >! timeout close! put!]]
+            [cljs.nodejs :as nodejs])
+  (:require-macros [cljs.core.async.macros :refer [go alt!]]))
+
+#_(do (ns butler.clojure.repl)
+      (require '[cljs.repl.node :as node])
+      (node/run-node-repl))
+
 
 (def http (nodejs/require "http"))
 
+(defn request [options]
+  (let [res-c (chan)]
+    (-> (.request http (clj->js options)
+                  (fn [res]
+                    (let [res-a (atom {:status (-> res .-statusCode)})]
+                      (.setEncoding res "utf8")
+                      (.on res "data" #(swap! res-a update-in [:data] str %))
+                      (.on res "end" #(go (>! res-c @res-a))))))
+        (.on "error" #(go (>! res-c {:error (-> % .-message)})))
+        (.end))
+    res-c))
+
 (defn greetHandler [req res]
-  (doto res
-    (.writeHead 200 {"Content-Type" "text/plain"})
-    (.end (com/GET "http://localhost:5984/nitro-books"))))
+  (.writeHead res 200 {"Content-Type" "text/plain"})
+  (println "HEADERS: " (js->clj (.-headers req) {:keywordize-keys true}))
+  (println "METHOD: " (.-method req))
+  (println "URL: " (.-url req))
+  (go (.end res (str (<! (request {:hostname "localhost"
+                                   :port 5984
+                                   :path "/nitro-books"}))))))
 
 (defn server [handler port url]
   (-> (.createServer http handler)
       (.listen port url)))
 
-(defn -main [& mess]
+(defn -main [& args]
   (server greetHandler 1337 "127.0.0.1")
   (println "Server running at http://127.0.0.1:1337/"))
 
