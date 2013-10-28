d928ece6f544598004d929bd8ad6b63ba7f988b7
Christian Weilbach <christian_weilbach@web.de>
2013-10-28 22:24:36 +0100

    Form small ring like chain and implement routes manually.

--- src/butler/core.cljs
+++ src/butler/core.cljs
@@ -23,21 +23,40 @@
         (.end))
     res-c))
 
-(defn greetHandler [req res]
-  (.writeHead res 200 {"Content-Type" "text/plain"})
-  (println "HEADERS: " (js->clj (.-headers req) {:keywordize-keys true}))
-  (println "METHOD: " (.-method req))
-  (println "URL: " (.-url req))
-  (go (.end res (str (<! (request {:hostname "localhost"
-                                   :port 5984
-                                   :path "/nitro-books"}))))))
+(defn routing [handler & options]
+  (fn [req-chan]
+    (go (let [resp (<! (handler req-chan))]
+          (cond (re-find #"^/get" (:url resp))
+                (merge resp (<! (request {:hostname "localhost"
+                                          :port 5984
+                                          :path "/nitro-books/168bd6ab987b7161ffd61449ee000d2a"})))
+
+                :else
+                (assoc resp
+                  :status 404
+                  :data "Not found."))))))
+
+(defn ring [handler req res]
+  (let [ring-req (-> {:headers (js->clj (.-headers req) {:keywordize-keys true})}
+                     (assoc :method (.-method req))
+                     (assoc :url (.-url req)))
+        req-chan (go ring-req)]
+    (go (let [ring-res (<! (handler req-chan))]
+          (println "RES-REQ: " (str ring-res))
+          (.writeHead res
+                      (or (:status ring-res) 500)
+                      {"Content-Type" (or (:content-type ring-res) "text/plain")})
+          (.end res (str ring-res))))))
+
+(def app (-> identity
+             (routing)))
 
 (defn server [handler port url]
   (-> (.createServer http handler)
       (.listen port url)))
 
 (defn -main [& args]
-  (server greetHandler 1337 "127.0.0.1")
+  (server (partial ring app) 1337 "127.0.0.1")
   (println "Server running at http://127.0.0.1:1337/"))
 
 (set! *main-cli-fn* -main)
